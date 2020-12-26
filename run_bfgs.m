688aa0e28ca180bcffb37842e85a149e85b385cc
Wangshi <220041041@link.cuhk.edu.cn>
2020-12-26 18:44:43 +0800

    train and test complete

--- run_bfgs.m
+++ run_bfgs.m
@@ -2,12 +2,23 @@
 addpath(genpath('method'));
 addpath(genpath('function'));
 addpath(genpath('visualization'));
+addpath(genpath('search'));
+addpath(genpath('test'));
+% Global Seed Settings
+rng("default");
 
 %%%%%%%%%%%%%
 % Load data %
 %%%%%%%%%%%%%
 global data1;global label1;
 load("small\small_dataset_sample.mat");
+train_ratio = 0.8;len_data = length(data1);
+rand_index = randperm(len_data,len_data);
+train_index = rand_index(1:floor(len_data * train_ratio));
+test_index = rand_index((floor(len_data * train_ratio) + 1):len_data);
+%split dataset
+data2 = data1(:,test_index);label2 = label1(test_index);
+data1 = data1(:,train_index);label1 = label1(train_index);
 
 
 %%%%%%%%%%%%%%%%%%%
@@ -55,10 +66,22 @@ x0 = [0;0;0];
 
 
 %%%%%%%%%%%%%%%%%
+%     test      %
+%%%%%%%%%%%%%%%%%
+opt1.paras = x;
+%bar for SVM problem
+opt1.bar = 0;opt1.label_bar = 0;
+%bar for Logistic problem
+opt1.bar = 0.5;opt1.label_bar = 0.5;
+
+CR_train = cal_CR(data1,label1,opt1);
+CR_test = cal_CR(data2,label2,opt1);
+
+%%%%%%%%%%%%%%%%%
 % Visualization %
 %%%%%%%%%%%%%%%%%
 
-plot_scatter(data1,label1);
+plot_scatter(data2,label2);
 hold on
 
 x1 = -3:0.01:3;
